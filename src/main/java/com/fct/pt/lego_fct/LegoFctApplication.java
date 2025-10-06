ab459c5a95e21006976c35f059903ab5fb5729e2
sebasrusu <sebastiaotr.5@gmail.com>
2025-10-07 00:00:41 +0100

    Initial commit

---
+++ src/main/java/com/fct/pt/lego_fct/LegoFctApplication.java
@@ -0,0 +1,13 @@
+package com.fct.pt.lego_fct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoFctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoFctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fct/pt/lego_fct/LegoFctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fct.pt.lego_fct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoFctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
