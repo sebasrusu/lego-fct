24ada5e1c9e6e24108a643a3c2c3d0a2bff11c37
sebasrusu <sebastiaotr.5@gmail.com>
2025-11-01 19:48:59 +0000

    Close auction feito

--- src/cc/functions/CloseAuctionsFunction.java
+++ src/cc/functions/CloseAuctionsFunction.java
@@ -1,24 +1,25 @@
 package cc.functions;
 
 import cc.data.auction.AuctionDAO;
+import cc.db.CosmosDBLayer;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.*;
 import com.azure.cosmos.*;
 import com.azure.cosmos.models.*;
 
+import java.util.logging.Logger;
+
 public class CloseAuctionsFunction {
+    String url = System.getenv("DB_URL");
+    String key = System.getenv("DB_KEY");
+    String dbName = System.getenv("DB_NAME");
 
     @FunctionName("CloseExpiredAuctions")
-    public void run(
+    public void runExpiredAuctions(
             @TimerTrigger(name = "timer", schedule = "0 */10 * * * *") String timerInfo,
             ExecutionContext context
     ) {
         context.getLogger().info("=== FECHAR LEILÕES EXPIRADOS ===");
-
-        String url = System.getenv("DB_URL");
-        String key = System.getenv("DB_KEY");
-        String dbName = System.getenv("DB_NAME");
-
         CosmosClient client = new CosmosClientBuilder()
                 .endpoint(url)
                 .key(key)
@@ -38,5 +39,26 @@ public class CloseAuctionsFunction {
         }
 
         client.close();
+    }
+
+    @FunctionName("CloseAuctions")
+    public void runCloseAuctions(
+            @TimerTrigger(name = "timer", schedule = "0 */5 * * * *") String timerInfo,
+            final ExecutionContext context){
+        Logger logger = context.getLogger();
+        logger.info("CloseAuctions function triggered: " + timerInfo);
+
+        CosmosDBLayer db = null;
+        try {
+            db = CosmosDBLayer.getInstance();
+            var expiredAuctions = db.listExpiredAuctions();
+            for (AuctionDAO auction : expiredAuctions) {
+                logger.info("Closing auction: " + auction.getId());
+                auction.setClosed(true);
+                db.updateAuction(auction);
+            }
+        } catch (Exception e) {
+            logger.severe("Error in CloseAuctions function: " + e.getMessage());
+        }
     }
 }
