7c3e8427c5e7be71d59815f3555e757ce201ef49
sebasrusu <sebastiaotr.5@gmail.com>
2025-11-08 01:46:19 +0000

    nova função para functions

--- functions/src/cc/functions/CloseAuctionsFunction.java
+++ functions/src/cc/functions/CloseAuctionsFunction.java
@@ -3,41 +3,10 @@ package cc.functions;
 import cc.data.auction.AuctionDAO;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.*;
-import com.azure.cosmos.*;
-import com.azure.cosmos.models.*;
 import java.util.logging.Logger;
 import cc.db.CosmosDBLayer;
 
 public class CloseAuctionsFunction {
-
-    @FunctionName("CloseExpiredAuctions")
-    public void run(
-            @TimerTrigger(name = "closeExpiredAuctionsTimer", schedule = "0 */10 * * * *") String timerInfo,
-            ExecutionContext context
-    ) {
-        context.getLogger().info("=== FECHAR LEILÕES EXPIRADOS ===");
-
-        String url = System.getenv("DB_URL");
-        String key = System.getenv("DB_KEY");
-        String dbName = System.getenv("DB_NAME");
-
-        CosmosClient client = new CosmosClientBuilder()
-                .endpoint(url)
-                .key(key)
-                .buildClient();
-
-        CosmosContainer auctions = client.getDatabase(dbName).getContainer("auctions");
-
-        long now = System.currentTimeMillis();
-        String query = "SELECT * FROM c WHERE c.closeDate < " + now;
-
-        for (var item : auctions.queryItems(query, new CosmosQueryRequestOptions(), AuctionDAO.class)) {
-            context.getLogger().info("Leilão fechado: " + item.getId());
-            item.setClosed(true);
-            auctions.upsertItem(item);
-        }
-        client.close();
-    }
 
     @FunctionName("CloseAuctions")
     public void runCloseAuctions(
@@ -56,6 +25,23 @@ public class CloseAuctionsFunction {
             }
         } catch (Exception e) {
             logger.severe("Error in CloseAuctions function: " + e.getMessage());
+        }
+    }
+
+    @FunctionName("GarbageCollection")
+    public void runGarbageCollection(
+            @TimerTrigger(name = "garbageCollectionTimer", schedule = "0 */4 * * * *") String timerInfo, // Executa a cada 4 minutos
+            final ExecutionContext context) {
+        
+        Logger logger = context.getLogger();
+        logger.info("=== GARBAGE COLLECTION: Limpando sessões expiradas ===");
+
+        try {
+            CosmosDBLayer db = CosmosDBLayer.getInstance();
+            int deletedCount = db.deleteExpiredSessions();
+            logger.info(String.format("Garbage Collection concluído. %d sessões expiradas foram removidas.", deletedCount));
+        } catch (Exception e) {
+            logger.severe("Erro durante o Garbage Collection: " + e.getMessage());
         }
     }
 }
